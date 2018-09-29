7466f63db7e6d8e031669611f3efefbb4d1a7c90
Skandar Ln <715421045@qq.com>
2018-09-29 23:55:58 +0800

    调整项目结构， 增加图片上传加载状态

--- src/service/oss/conf.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const {oss} = require('../../../config.json');
-
-module.exports = {
-        ...oss.sts,
-        // "TokenExpireTime": "900",
-        "PolicyFile": path.resolve(__dirname, './policy.txt')
-}
