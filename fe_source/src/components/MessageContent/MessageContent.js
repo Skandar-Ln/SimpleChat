23ac2c613c91c2af3f7fe2ae23c8c7cee29a62c9
Skandar <oo0o0o0qq0@outlook.com>
2018-10-02 15:40:56 +0800

    支持链接渲染

--- fe_source/src/components/MessageContent/MessageContent.js
+++ fe_source/src/components/MessageContent/MessageContent.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
 import AsyncImg from '../AsyncImg';
+import startsWith from 'lodash/startsWith';
+
+const urlRegex = /\b\S+\.(com|wang|net|me|cn|fun)\S*\b/g;
+
+const renderContent = content => {
+    if (!urlRegex.test(content)) {
+        return content;
+    }
+
+    const urledContnet = content.replace(urlRegex, function(match) {
+        const a = document.createElement('a');
+        const href = startsWith(match, 'http') ? href : `//${match}`;
+        a.setAttribute('href', href);
+        a.setAttribute('target', '_blank');
+        a.innerText = match;
+
+        return a.outerHTML;
+    });
+
+    return (
+        <div dangerouslySetInnerHTML={{__html:urledContnet}}></div>  
+    );
+}
 
 export default function({content = '', type}) {
     if (type === 'img') {
@@ -9,5 +32,5 @@ export default function({content = '', type}) {
     return <span style={{
         wordBreak: 'break-word',
         fontSize: 16
-    }}>{content}</span>;
+    }}>{renderContent(content)}</span>;
 }
