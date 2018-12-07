cf262a0a63cbd9091ced17f18af5ec81ea5e8dbc
Skandar <oo0o0o0qq0@outlook.com>
2018-12-07 16:47:27 +0800

    add sentry

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import * as Sentry from '@sentry/browser';
+
 import Chat from './components/Chat';
 
 // import logo from './logo.svg';
 import './App.css';
 
+Sentry.init({
+ dsn: "https://19483dd454cd45849bdde4ce2763a1bc@sentry.io/1339453"
+});
+
 class App extends Component {
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+      Sentry.captureException(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
